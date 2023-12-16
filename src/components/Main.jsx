70268b57a42d5aa95fa246e8e8132fc20e1cec73
edgarmuyomba <edgarmatthew247@gmail.com>
2023-12-16 15:24:54 +0300

    clear cv

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,13 @@
 import CV from "./CV";
+import Clear from "./clear";
 
 export default function Main({ resume_content, updateResume }) {
     return (
-        <main>
-            <CV updateResume={updateResume} resume={resume_content} />
-        </main>
+        <>
+            <main>
+                <CV updateResume={updateResume} resume={resume_content} />
+            </main>
+            <Clear updateContent={updateResume} />
+        </>
     );
 };
